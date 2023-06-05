1bf3ce17a75ab0b744ca71750b73a1fa82a965e7
Krazza <muondove@gmail.com>
2023-06-05 17:47:24 +0300

    3.4 done

--- index.js
+++ index.js
@@ -21,6 +21,11 @@ let persons = [
       "id": 4,
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
+    },
+    { 
+      "id": 5,
+      "name": "Kev", 
+      "number": "123"
     }
 ];
 
@@ -47,6 +52,12 @@ app.get("/api/persons/:id", (request, response) => {
     }
     
 });
+
+app.delete(`/api/persons/:id`, (request, response) => {
+    const id = Number(request.params.id);
+    persons = persons.filter(person => person.id !== id);
+    response.status(204).end();
+})
 
 const PORT = 3005;
 app.listen(PORT, () =>{
