45b8236d6e50da0dd51dfb00292496d3ca959698
Krazza <muondove@gmail.com>
2023-06-05 19:41:02 +0300

    3.8 done

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+
+morgan.token('body', req => {
+    if(req.method == "POST")
+        return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'));
 
 let persons = [
     { 
